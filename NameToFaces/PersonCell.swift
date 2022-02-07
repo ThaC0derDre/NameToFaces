c681cbe4d4edab1c55c0a35ff3d8001e1af1127e
AndresGutierrez <c0dingw.dre@gmail.com>
2022-02-07 00:14:35 -0800

    create custom cell class, and type cast to cell for items

---
+++ NameToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NameToFaces
+//
+//  Created by Andres Gutierrez on 2/6/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
--- NameToFaces/ViewController.swift
+++ NameToFaces/ViewController.swift
@@ -15,5 +15,16 @@ class ViewController: UICollectionViewController {
     }
 
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Error, could not make cell a PersonCell")
+        }
+        return cell
+    }
+    
 }
 
